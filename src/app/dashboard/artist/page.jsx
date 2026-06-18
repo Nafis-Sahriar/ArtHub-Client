667910f46ca2f7d1d17fb0c505387e7b6c5a17f8
Nafis-Sahriar <nafis.apid@gmail.com>
2026-06-18 09:50:26 +0600

    Got the session in Dashboard Sidebar, and written the initial Welcome Message

--- src/app/dashboard/artist/page.jsx
+++ src/app/dashboard/artist/page.jsx
@@ -1,9 +1,24 @@
+'use client';
+import { useSession } from '@/lib/auth-client';
 import React from 'react';
 
 const ArtistHomePage = () => {
+
+
+    const {data:session, isPending} = useSession();
+
+    if(isPending) {
+        return <div>Loading...</div>;
+    }
+    const user = session?.user;
+    console.log("User session data:", user);
+
     return (
         <div>
-            I am an artist.
+            <h1 className="text-3xl font-bold mb-4">Welcome, {user?.name || "Artist"}!</h1>
+            <p className="text-lg text-gray-600">
+                This is your artist dashboard where you can manage your artworks, view sales history, and update your profile.
+            </p>
         </div>
     );
 };
