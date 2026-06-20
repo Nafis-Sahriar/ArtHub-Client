a1ac78172abf92adef8e228904af67554056ef18
Nafis-Sahriar <nafis.apid@gmail.com>
2026-06-20 22:15:06 +0600

    Generated JWT Token

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGO_DB_URI);
 const db = client.db(process.env.AUTH_DB_NAME);
@@ -33,6 +34,17 @@ export const auth = betterAuth({
         }
     },
 
+    session:{
+      cookieCache:{
+        enabled:true,
+        strategy:'jwt',
+        maxAge:60*60*24*7, 
+      },
+    },
+  
+    plugins:[
+      jwt()
+    ]
 
 
 
